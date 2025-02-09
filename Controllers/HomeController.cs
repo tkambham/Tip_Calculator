587924f41d086d69aef95c3684999a589d70d09d
trilok <trilokkambham2001@gmail.com>
2025-02-09 16:34:30 -0600

    update 1.5(controller code updated)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Calculator_Project.Models;
 
 namespace Calculator_Project.Conrollers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Tip15 = 0;
+            ViewBag.Tip20 = 0;
+            ViewBag.Tip25 = 0;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            ViewBag.Tip15 = model.CalculatePercent15();
+            ViewBag.Tip20 = model.CalculatePercent20();
+            ViewBag.Tip25 = model.CalculatePercent25();
+
+            return View(model);
         }
     }
 }
